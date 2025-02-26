469faa01e4feccad6556c004c58f14f875fe5143
Cameron Jones <cjones.prsvr@gmail.com>
2025-02-25 21:40:25 -0600

    the start up

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+  
+  return (
+    <>
+      {/* add login */}
+      {/* add register */}
+    </>
+  );
+}
+
+export default App;
